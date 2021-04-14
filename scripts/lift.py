bf918125f29ae4995e335e87bd961ba133b612d3
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2021-04-14 10:46:48 +0300

    Update lift.py

--- scripts/lift.py
+++ scripts/lift.py
@@ -16,6 +16,7 @@ def boostrap():
         "scala": defaultdict(lambda: [])
     }
 
+
 DURATION = boostrap()
 LINES = boostrap()
 FILES = boostrap()
@@ -32,20 +33,23 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Lift correlations')
     parser.add_argument(
-        "iterations",
-        help="Directory that contains the iterations.")
-    parser.add_argument(
         "bugs",
-        help="Directory that contains bugs.")
+        help="File with the bugs")
+    parser.add_argument(
+        "stats", help="Directory that contains the stats for the bugs.")
     parser.add_argument(
         "diffs", help="Directory that contains the diffs of the bug fixes.")
-    parser.add_argument("--threshold", type=float, help="Threshold for lift.")
+    parser.add_argument("--threshold", type=float,
+                        default=5,
+                        help="Threshold for lift.")
     parser.add_argument("--ithreshold", type=int,
+                        default=10,
                         help="Intersections threshold for lift.")
-    parser.add_argument("--root-causes", help="JSON with root causes")
-    parser.add_argument("--characteristics", help="JSON with characteristics")
-    parser.add_argument("--symptoms", help="JSON with symptoms")
-    parser.add_argument("--categories", help="JSON with categories")
+    parser.add_argument("--limit", type=int,
+                        default=5,
+                        help="Max entries to show per pair.")
+    parser.add_argument("--all", action='store_true',
+                        help="Print lift score for all categories")
     return parser.parse_args()
 
 
@@ -82,139 +86,78 @@ def duration_lookup(duration):
 
 
 def read_data(args):
-    root_causes = {}
-    if args.root_causes:
-        with open(args.root_causes, 'r') as fp:
-            root_causes = json.load(fp)
-    characteristics = {}
-    if args.characteristics:
-        with open(args.characteristics, 'r') as fp:
-            characteristics = json.load(fp)
-    symptoms = {}
-    if args.symptoms:
-        with open(args.symptoms, 'r') as fp:
-            symptoms = json.load(fp)
-    categories = {}
-    if args.categories:
-        with open(args.categories, 'r') as fp:
-            categories = json.load(fp)
-    for d in list(os.walk(args.iterations))[1:]:
-        if "4" in d[0]:
-            continue
-        for f in d[2]:
-            filename = os.path.join(d[0], f)
-            with open(filename) as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter=',')
-                for row in csv_reader:
-                    lang = ""
-                    bug = ""
-                    if "JDK" in row[0]:
-                        lang = "java"
-                        bug = row[0].split("/")[-1]
-                    elif "GROOVY" in row[0]:
-                        lang = "groovy"
-                        bug = row[0].split("/")[-1]
-                    elif "KT" in row[0]:
-                        lang = "kotlin"
-                        bug = row[0].split("/")[-1]
-                    elif "scala" in row[0]:
-                        lang = "scala"
-                        bug = "scala-" + row[0].split("/")[-1]
-                    elif "dotty" in row[0]:
-                        lang = "scala"
-                        bug = "dotty-" + row[0].split("/")[-1]
-                    with open(os.path.join(args.bugs, lang + ".json")) as jf:
-                        stats = json.load(jf)
-                    # Duration
-                    try:
-                        created = stats[bug]["created"]
-                        resolution = stats[bug]["resolution"]
-                    except KeyError:
-                        print("Warning {} not found (duration)".format(bug))
-                        continue
-                    try:
-                        created = datetime.strptime(
-                            created, '%Y-%m-%d %H:%M:%S%z')
-                        resolution = datetime.strptime(
-                            resolution, '%Y-%m-%d %H:%M:%S%z')
-                    except ValueError:
-                        created = datetime.strptime(
-                            created, '%Y-%m-%d %H:%M:%S.%f')
-                        resolution = datetime.strptime(
-                            resolution, '%Y-%m-%d %H:%M:%S.%f')
-                    duration = (resolution - created).days
-                    cluster = duration_lookup(duration)
-                    # Lines - Files
-                    diffstats = os.path.join(args.diffs, lang, bug, "stats.csv")
-                    try:
-                        with open(diffstats) as csv_file:
-                            csv_reader = csv.reader(csv_file, delimiter=',')
-                            # skip column names: INSERTED,DELETED,MODIFIED,FILENAME
-                            next(csv_reader)
-                            fix_lines = 0
-                            fix_files = 0
-                            for row in csv_reader:
-                                if (any(x in row[3] for x in KEYWORDS) or
-                                        not any(x in row[3]
-                                            for x in SOURCE_FILES)):
-                                    continue
-                                fix_files += 1
-                                fix_lines += int(row[0]) + int(row[1])
-                            lines_cluster = lines_lookup(fix_lines)
-                            files_cluster = files_lookup(fix_files)
-                    except FileNotFoundError:
-                        print("Warning {} not found (file)".format(bug))
-                        continue
+    with open(args.bugs) as jf:
+        bugs = json.load(jf)
+    for l in ("groovy", "java", "kotlin", "scala"):
+        with open(os.path.join(args.stats, l + ".json")) as jf:
+            stats = json.load(jf)
+            for bug, values in stats.items():
+                # Duration
+                created = stats[bug]["created"]
+                resolution = stats[bug]["resolution"]
+                try:
+                    created = datetime.strptime(
+                        created, '%Y-%m-%d %H:%M:%S%z')
+                    resolution = datetime.strptime(
+                        resolution, '%Y-%m-%d %H:%M:%S%z')
+                except ValueError:
+                    created = datetime.strptime(
+                        created, '%Y-%m-%d %H:%M:%S.%f')
+                    resolution = datetime.strptime(
+                        resolution, '%Y-%m-%d %H:%M:%S.%f')
+                duration = (resolution - created).days
+                cluster = duration_lookup(duration)
+                # Lines - Files
+                diffstats = os.path.join(args.diffs, l, bug, "stats.csv")
+                try:
+                    with open(diffstats) as csv_file:
+                        csv_reader = csv.reader(csv_file, delimiter=',')
+                        # skip column names: INSERTED,DELETED,MODIFIED,FILENAME
+                        next(csv_reader)
+                        fix_lines = 0
+                        fix_files = 0
+                        for row in csv_reader:
+                            if (any(x in row[3] for x in KEYWORDS) or
+                                    not any(x in row[3]
+                                    for x in SOURCE_FILES)):
+                                continue
+                            fix_files += 1
+                            fix_lines += int(row[0]) + int(row[1])
+                        lines_cluster = lines_lookup(fix_lines)
+                        files_cluster = files_lookup(fix_files)
+                except FileNotFoundError:
+                    print("Warning {} not found (file)".format(bug))
+                    continue
+                bug = bug.replace('dotty', 'Dotty').replace('scala', 'Scala2')
+                try:
                     DURATION["total"][cluster].append(bug)
-                    DURATION[lang][cluster].append(bug)
+                    DURATION[l][cluster].append(bug)
                     LINES["total"][lines_cluster].append(bug)
-                    LINES[lang][lines_cluster].append(bug)
+                    LINES[l][lines_cluster].append(bug)
                     FILES["total"][files_cluster].append(bug)
-                    FILES[lang][files_cluster].append(bug)
-                    if args.root_causes:
-                        try:
-                            root_cause = root_causes[bug][0]
-                            ROOT_CAUSES["total"][root_cause].append(bug)
-                            ROOT_CAUSES[lang][root_cause].append(bug)
-                        except KeyError:
-                            print("Warning {} not found (root_cause)".format(
-                                bug))
-                            continue
-                    if args.categories:
-                        try:
-                            category = categories[bug][0]
-                            CATEGORIES["total"][category].append(bug)
-                            CATEGORIES[lang][category].append(bug)
-                        except KeyError:
-                            print("Warning {} not found (category)".format(
-                                bug))
-                            continue
-                    if args.symptoms:
-                        try:
-                            symptom = symptoms[bug][0]
-                            SYMPTOMS["total"][symptom].append(bug)
-                            SYMPTOMS[lang][symptom].append(bug)
-                        except KeyError:
-                            print("Warning {} not found (symptom)".format(
-                                bug))
-                            continue
-                    if args.characteristics:
-                        try:
-                            chars = characteristics[bug]["characteristics"]
-                            char_categories = characteristics[bug]["categories"]
-                            for char in chars:
-                                CHARACTERISTICS["total"][char].append(bug)
-                                CHARACTERISTICS[lang][char].append(bug)
-                            for cat in char_categories:
-                                CHARACTERISTICS_CATEGORIES["total"][cat].append(bug)
-                                CHARACTERISTICS_CATEGORIES[lang][cat].append(bug)
-                        except KeyError:
-                            print("Warning {} not found (char)".format(
-                                bug))
-                            continue
+                    FILES[l][files_cluster].append(bug)
+                    root_cause = bugs[bug]['root_cause']['category']
+                    ROOT_CAUSES["total"][root_cause].append(bug)
+                    ROOT_CAUSES[l][root_cause].append(bug)
+                    category = bugs[bug]['pattern']['category']
+                    CATEGORIES["total"][category].append(bug)
+                    CATEGORIES[l][category].append(bug)
+                    symptom = bugs[bug]['symptom']
+                    SYMPTOMS["total"][symptom].append(bug)
+                    SYMPTOMS[l][symptom].append(bug)
+                    chars = bugs[bug]['chars']['characteristics']
+                    char_categories = bugs[bug]['chars']['categories']
+                    for char in chars:
+                        CHARACTERISTICS["total"][char].append(bug)
+                        CHARACTERISTICS[l][char].append(bug)
+                    for cat in char_categories:
+                        CHARACTERISTICS_CATEGORIES["total"][cat].append(bug)
+                        CHARACTERISTICS_CATEGORIES[l][cat].append(bug)
+                except KeyError:
+                    print("Cannot find {}".format(bug))
 
 
-def compute(keyword, a, b, threshold=False, ithreshold=False):
+def compute(keyword, a, b, threshold=False, ithreshold=False, limit=None):
     """https://stackabuse.com/association-rule-mining-via-apriori-algorithm-in-python/
     """
     size = sum(len(i) for i in a["total"].values())
@@ -233,6 +176,7 @@ def compute(keyword, a, b, threshold=False, ithreshold=False):
     lift_a_b = defaultdict(lambda: [])
     max_a = max(len(i.split("->")[0]) for i in confidence_a_b.keys())
     max_b = max(len(i.split("->")[1]) for i in confidence_a_b.keys())
+    res = []
     for key, confidence in confidence_a_b.items():
         b_key = key.split("->")[-1]
         lift = confidence / support_b[b_key]
@@ -246,42 +190,59 @@ def compute(keyword, a, b, threshold=False, ithreshold=False):
             continue
         if a_key == b_key:
             continue
+        res.append((a_key, b_key, lift, confidence, support_b[b_key],
+                    len(a[keyword][a_key]), len(b[keyword][b_key]),
+                    intersection, max_a, max_b))
+    res.sort(reverse=True, key=lambda x: x[2])
+    res = res[:limit] if limit else res
+    for r in res:
         print(("Lift {:>{x}} -> {:<{y}}: {:.4f} (Confidence A->B: {:.4f}, "
                "Support B: {:.4f}) -- Totals A: {}, B: {}, A-B: {}").format(
-                a_key, b_key, lift, confidence, support_b[b_key],
-                len(a[keyword][a_key]), len(b[keyword][b_key]),
+                r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7],
                 intersection,
-                x=max_a, y=max_b))
+                x=r[8], y=r[9]))
 
 
 def main():
     args = get_args()
     read_data(args)
-    print("Duration -> Lines")
-    compute("total", DURATION, LINES, args.threshold, args.ithreshold)
-    print("Lines -> Duration")
-    compute("total", LINES, DURATION, args.threshold, args.ithreshold)
-    if args.root_causes:
+    if args.all:
+        print("Duration -> Lines")
+        compute("total", DURATION, LINES, args.threshold, args.ithreshold,
+                args.limit)
+        print("Lines -> Duration")
+        compute("total", LINES, DURATION, args.threshold, args.ithreshold,
+                args.limit)
         print("Root cause -> Duration")
-        compute("total", ROOT_CAUSES, DURATION, args.threshold, args.ithreshold)
+        compute("total", ROOT_CAUSES, DURATION, args.threshold,
+                args.ithreshold, args.limit)
         print("Root cause -> Lines")
-        compute("total", ROOT_CAUSES, LINES, args.threshold, args.ithreshold)
+        compute("total", ROOT_CAUSES, LINES, args.threshold, args.ithreshold,
+                args.limit)
         print("Root cause -> FILES")
-        compute("total", ROOT_CAUSES, FILES, args.threshold, args.ithreshold)
-    if args.characteristics:
+        compute("total", ROOT_CAUSES, FILES, args.threshold, args.ithreshold,
+                args.limit)
         print("Characteristics -> Characteristics")
         compute("total", CHARACTERISTICS, CHARACTERISTICS, args.threshold,
-                args.ithreshold)
+                args.ithreshold, args.limit)
         print("Char Categories -> Char Categories")
         compute("total", CHARACTERISTICS_CATEGORIES,
-                CHARACTERISTICS_CATEGORIES, args.threshold, args.ithreshold)
-    if args.characteristics and args.symptoms:
+                CHARACTERISTICS_CATEGORIES, args.threshold, args.ithreshold,
+                args.limit)
         print("Symptom -> Characteristics")
         compute("total", SYMPTOMS, CHARACTERISTICS, args.threshold,
-                args.ithreshold)
-    if args.root_causes and args.categories:
+                args.ithreshold, args.limit)
         print("Category -> Root Cause")
-        compute("total", CATEGORIES, ROOT_CAUSES, args.threshold, args.ithreshold)
+        compute("total", CATEGORIES, ROOT_CAUSES, args.threshold,
+                args.ithreshold, args.limit)
+    else:
+        print("Char Categories -> Char Categories")
+        compute("total", CHARACTERISTICS_CATEGORIES,
+                CHARACTERISTICS_CATEGORIES, args.threshold,
+                args.ithreshold, args.limit)
+        print("Characteristics -> Characteristics")
+        compute("total", CHARACTERISTICS, CHARACTERISTICS, args.threshold,
+                args.ithreshold, args.limit)
 
 
 if __name__ == "__main__":
